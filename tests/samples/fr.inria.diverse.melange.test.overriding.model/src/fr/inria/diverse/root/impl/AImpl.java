41c60b3cd4ff718ae71954df86f74f2cc4de9b79
Fabien Coulon <fabien.coulon@inria.fr>
2015-09-22 11:30:12 +0200

    Add a sample project to test Aspects overriding

---
+++ tests/samples/fr.inria.diverse.melange.test.overriding.model/src/fr/inria/diverse/root/impl/AImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package fr.inria.diverse.root.impl;
+
+import fr.inria.diverse.root.A;
+import fr.inria.diverse.root.RootPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>A</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class AImpl extends MinimalEObjectImpl.Container implements A {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected AImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return RootPackage.Literals.A;
+	}
+
+} //AImpl
