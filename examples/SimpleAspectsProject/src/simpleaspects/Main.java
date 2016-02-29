a5cf139090ea05df7e70b4551c2dec2e7d0d4792
Thomas Degueule <thomas.degueule@inria.fr>
2016-02-29 11:00:39 +0100

    Example of reflexive model manipulation on a model type for SimpleAspects project

--- examples/SimpleAspectsProject/src/simpleaspects/Main.java
+++ examples/SimpleAspectsProject/src/simpleaspects/Main.java
@@ -1,6 +1,7 @@
 package simpleaspects;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -14,6 +15,13 @@ public class Main {
 		System.out.println();
 	}
 
+	public static void reflexivePrettyPrint(EObject o) {
+		o.eClass().getEStructuralFeatures().forEach(f -> {
+			System.out.println(f.getName() + " = " + o.eGet(f));
+			o.eContents().forEach(c -> reflexivePrettyPrint(c));
+		});
+	}
+
 	public static void main(String[] args) {
 		StandaloneSetup.doSetup();
 		ResourceSet rs = new ResourceSetImpl();
@@ -24,5 +32,8 @@ public class Main {
 
 		execute((FSM) fsmRes.getContents().get(0));
 		execute((FSM) tfsmRes.getContents().get(0));
+		System.out.println();
+		reflexivePrettyPrint(fsmRes.getContents().get(0));
+		reflexivePrettyPrint(tfsmRes.getContents().get(0));
 	}
 }
