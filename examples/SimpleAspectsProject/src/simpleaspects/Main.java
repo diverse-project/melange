093ff92ab6769b1916fcac6916c782d6faa0aa8d
Thomas Degueule <thomas.degueule@inria.fr>
2016-03-03 16:38:38 +0100

    SimpleAspectsProject: example of polymorphic ATL & QVTo invocations

--- examples/SimpleAspectsProject/src/simpleaspects/Main.java
+++ examples/SimpleAspectsProject/src/simpleaspects/Main.java
@@ -1,11 +1,15 @@
 package simpleaspects;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.EList;
@@ -15,6 +19,19 @@ import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.m2m.atl.core.ATLCoreException;
+import org.eclipse.m2m.atl.core.IExtractor;
+import org.eclipse.m2m.atl.core.IInjector;
+import org.eclipse.m2m.atl.core.IModel;
+import org.eclipse.m2m.atl.core.IReferenceModel;
+import org.eclipse.m2m.atl.core.ModelFactory;
+import org.eclipse.m2m.atl.core.emf.EMFExtractor;
+import org.eclipse.m2m.atl.core.emf.EMFInjector;
+import org.eclipse.m2m.atl.core.emf.EMFModel;
+import org.eclipse.m2m.atl.core.emf.EMFModelFactory;
+import org.eclipse.m2m.atl.core.emf.EMFReferenceModel;
+import org.eclipse.m2m.atl.core.launch.ILauncher;
+import org.eclipse.m2m.atl.engine.emfvm.launch.EMFVMLauncher;
 import org.eclipse.m2m.qvt.oml.BasicModelExtent;
 import org.eclipse.m2m.qvt.oml.ExecutionContextImpl;
 import org.eclipse.m2m.qvt.oml.ExecutionDiagnostic;
@@ -32,6 +49,18 @@ public class Main {
 		System.out.println();
 	}
 
+	public static void prettyPrint(FSM fsm) {
+		fsm.getOwnedState().forEach(s -> {
+			System.out.println("State" + s.getName());
+			s.getIncomingTransition().forEach(t -> {
+				System.out.println("\tIncoming " + t.getInput() + "(" + t.getOutput() + ")");
+			});
+			s.getOutgoingTransition().forEach(t -> {
+				System.out.println("\tOutgoing " + t.getInput() + "(" + t.getOutput() + ")");
+			});
+		});
+	}
+
 	public static void reflexivePrettyPrint(EObject o) {
 		o.eClass().getEStructuralFeatures().forEach(f -> {
 			System.out.println(f.getName() + " = " + o.eGet(f));
@@ -39,7 +68,10 @@ public class Main {
 		});
 	}
 
-	public static void runQVTo(EList<EObject> inObjects) {
+	public static void runQVTo(String inputURI, String outputURI) {
+		ResourceSet rs = new ResourceSetImpl();
+		Resource inRes = rs.getResource(URI.createURI(inputURI), true);
+		EList<EObject> inObjects = inRes.getContents();
 		URI transformationURI = URI.createFileURI(
 			"/home/dig/git/melange/examples/SimpleFsmQVTo/transforms/DummyInvert.qvto");
 		TransformationExecutor executor = new TransformationExecutor(transformationURI);
@@ -53,10 +85,8 @@ public class Main {
 		ExecutionDiagnostic result = executor.execute(context, input, output);
 
 		if(result.getSeverity() == Diagnostic.OK) {
-			System.out.println("QVTo successfully invoked!");
 			List<EObject> outObjects = output.getContents();
-			ResourceSet rs = new ResourceSetImpl();
-			URI outUri = URI.createURI("/home/dig/git/melange/examples/SimpleAspectsProject/output/out.fsm");
+			URI outUri = URI.createURI("output/" + outputURI);
 			Resource res = rs.createResource(outUri);
 			res.getContents().addAll(outObjects);
 			try {
@@ -70,6 +100,42 @@ public class Main {
 		}
 	}
 
+	public static void runATL(String inputURI, String outputURI) {
+		try {
+			ILauncher transformationLauncher = new EMFVMLauncher();
+			ModelFactory modelFactory = new EMFModelFactory();
+			IInjector injector = new EMFInjector();
+			IExtractor extractor = new EMFExtractor();
+
+			IReferenceModel fsmmt = modelFactory.newReferenceModel();
+			injector.inject(fsmmt, "http://fsmmt/");
+			IReferenceModel fsm = modelFactory.newReferenceModel();
+			injector.inject(fsm, "http://fsm/");
+
+			IModel inModel = modelFactory.newModel(fsmmt);
+			injector.inject(inModel, inputURI);
+
+			IModel outModel = modelFactory.newModel(fsm);
+			transformationLauncher.initialize(new HashMap<String,Object>());
+			transformationLauncher.addInModel(inModel, "IN", "FsmMT");
+			transformationLauncher.addOutModel(outModel, "OUT", "Fsm");
+			transformationLauncher.launch(ILauncher.RUN_MODE, new NullProgressMonitor(), new HashMap<String,Object>(),
+				new FileInputStream("../SimpleFsmATL/DummyInvert.asm"));
+
+			extractor.extract(outModel, "output/" + outputURI);
+
+			EMFModelFactory emfModelFactory = (EMFModelFactory) modelFactory;
+			emfModelFactory.unload((EMFModel) inModel);
+			emfModelFactory.unload((EMFModel) outModel);
+			emfModelFactory.unload((EMFReferenceModel) fsmmt);
+			emfModelFactory.unload((EMFReferenceModel) fsm);
+		} catch (ATLCoreException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		EPackage.Registry.INSTANCE.put(simpleaspects.fsmmt.fsm.FsmPackage.eNS_URI,
 				simpleaspects.fsmmt.fsm.FsmPackage.eINSTANCE);
@@ -80,12 +146,33 @@ public class Main {
 		Resource tfsmRes  = rs.getResource(
 			URI.createURI("melange:/file/input/Simple.timedfsm?mt=simpleaspects.FsmMT"), true);
 
+		// Polymorphic execution
+		System.out.println("execute:\n");
 		execute((FSM) fsmRes.getContents().get(0));
 		execute((FSM) tfsmRes.getContents().get(0));
-		System.out.println();
+
+		// Polymorphic prettyPrint
+		System.out.println("prettyPrint:\n");
+		prettyPrint((FSM) fsmRes.getContents().get(0));
+		prettyPrint((FSM) tfsmRes.getContents().get(0));
+
+		// Polymorphic reflexive prettyPrint
+		System.out.println("reflexivePrettyPrint:\n");
 		reflexivePrettyPrint(fsmRes.getContents().get(0));
 		reflexivePrettyPrint(tfsmRes.getContents().get(0));
-		System.out.println();
-		runQVTo(fsmRes.getContents());
+
+		// Polymorphic QVTo invocation
+		System.out.println("runQVTo:\n");
+		runQVTo("melange:/file/input/Simple.fsm?mt=simpleaspects.FsmMT",
+				"OutputQVToFsm.fsm");
+		runQVTo("melange:/file/input/Simple.timedfsm?mt=simpleaspects.FsmMT",
+				"OutputQVToTimedFsm.fsm");
+
+		// Polymorphic ATL invocation
+		System.out.println("runATL:\n");
+		runATL("melange:/file/input/Simple.fsm?mt=simpleaspects.FsmMT",
+				"OutputATLFsm.fsm");
+		runATL("melange:/file/input/Simple.timedfsm?mt=simpleaspects.FsmMT",
+				"OutputATLTimedFsm.fsm");
 	}
 }
