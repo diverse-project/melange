db54e2f07a262aa8c534584b76e48c37f08f3d8d
dvojtise <didier.vojtisek@inria.fr>
2017-05-09 11:10:44 +0200

    add bundle activation lazy

--- plugins/fr.inria.diverse.melange.ui/src/main/java/fr/inria/diverse/melange/ui/wizards/NewMelangeProjectWizard.java
+++ plugins/fr.inria.diverse.melange.ui/src/main/java/fr/inria/diverse/melange/ui/wizards/NewMelangeProjectWizard.java
@@ -173,7 +173,7 @@ public class NewMelangeProjectWizard extends AbstractNewProjectWizardWithTemplat
 			manifestChanger.addPluginDependency("fr.inria.diverse.melange.resource", "0.1.0", true, true);
 			manifestChanger.addPluginDependency("fr.inria.diverse.melange.adapters", "0.1.0", true, true);
 			manifestChanger.addPluginDependency("fr.inria.diverse.k3.al.annotationprocessor.plugin");
-			
+			manifestChanger.addAttributes("Bundle-ActivationPolicy", "lazy");
 			manifestChanger.commit();
 			createPlugInFile(project, monitor);
 			createBuildProperties(project, monitor);			
